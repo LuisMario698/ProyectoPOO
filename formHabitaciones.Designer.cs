96557780994c50088f61c38577c31de51c81a622
LuisMario698 <LuisMario698@hotmail.com>
2024-12-07 22:56:13 -0700

    borre botones de habitaciones

--- formHabitaciones.Designer.cs
+++ formHabitaciones.Designer.cs
@@ -32,9 +32,6 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvHabitaciones = new Guna.UI.WinForms.GunaDataGridView();
-            this.btnRegistrar = new Guna.UI.WinForms.GunaButton();
-            this.btnModificar = new Guna.UI.WinForms.GunaButton();
-            this.btnEliminar = new Guna.UI.WinForms.GunaButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHabitaciones)).BeginInit();
             this.SuspendLayout();
             // 
@@ -95,89 +92,11 @@
             this.dgvHabitaciones.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvHabitaciones.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             // 
-            // btnRegistrar
-            // 
-            this.btnRegistrar.AnimationHoverSpeed = 0.07F;
-            this.btnRegistrar.AnimationSpeed = 0.03F;
-            this.btnRegistrar.BackColor = System.Drawing.Color.Transparent;
-            this.btnRegistrar.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.btnRegistrar.BorderColor = System.Drawing.Color.Black;
-            this.btnRegistrar.BorderSize = 1;
-            this.btnRegistrar.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.btnRegistrar.FocusedColor = System.Drawing.Color.Empty;
-            this.btnRegistrar.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnRegistrar.ForeColor = System.Drawing.Color.White;
-            this.btnRegistrar.Image = null;
-            this.btnRegistrar.ImageSize = new System.Drawing.Size(20, 20);
-            this.btnRegistrar.Location = new System.Drawing.Point(104, 284);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.btnRegistrar.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btnRegistrar.OnHoverForeColor = System.Drawing.Color.White;
-            this.btnRegistrar.OnHoverImage = null;
-            this.btnRegistrar.OnPressedColor = System.Drawing.Color.Black;
-            this.btnRegistrar.Radius = 10;
-            this.btnRegistrar.Size = new System.Drawing.Size(124, 42);
-            this.btnRegistrar.TabIndex = 2;
-            // 
-            // btnModificar
-            // 
-            this.btnModificar.AnimationHoverSpeed = 0.07F;
-            this.btnModificar.AnimationSpeed = 0.03F;
-            this.btnModificar.BackColor = System.Drawing.Color.Transparent;
-            this.btnModificar.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.btnModificar.BorderColor = System.Drawing.Color.Black;
-            this.btnModificar.BorderSize = 1;
-            this.btnModificar.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.btnModificar.FocusedColor = System.Drawing.Color.Empty;
-            this.btnModificar.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnModificar.ForeColor = System.Drawing.Color.White;
-            this.btnModificar.Image = null;
-            this.btnModificar.ImageSize = new System.Drawing.Size(20, 20);
-            this.btnModificar.Location = new System.Drawing.Point(234, 284);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.btnModificar.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btnModificar.OnHoverForeColor = System.Drawing.Color.White;
-            this.btnModificar.OnHoverImage = null;
-            this.btnModificar.OnPressedColor = System.Drawing.Color.Black;
-            this.btnModificar.Radius = 10;
-            this.btnModificar.Size = new System.Drawing.Size(124, 42);
-            this.btnModificar.TabIndex = 3;
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.AnimationHoverSpeed = 0.07F;
-            this.btnEliminar.AnimationSpeed = 0.03F;
-            this.btnEliminar.BackColor = System.Drawing.Color.Transparent;
-            this.btnEliminar.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.btnEliminar.BorderColor = System.Drawing.Color.Black;
-            this.btnEliminar.BorderSize = 1;
-            this.btnEliminar.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.btnEliminar.FocusedColor = System.Drawing.Color.Empty;
-            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnEliminar.ForeColor = System.Drawing.Color.White;
-            this.btnEliminar.Image = null;
-            this.btnEliminar.ImageSize = new System.Drawing.Size(20, 20);
-            this.btnEliminar.Location = new System.Drawing.Point(364, 284);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.btnEliminar.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btnEliminar.OnHoverForeColor = System.Drawing.Color.White;
-            this.btnEliminar.OnHoverImage = null;
-            this.btnEliminar.OnPressedColor = System.Drawing.Color.Black;
-            this.btnEliminar.Radius = 10;
-            this.btnEliminar.Size = new System.Drawing.Size(124, 42);
-            this.btnEliminar.TabIndex = 4;
-            // 
             // formHabitaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(580, 367);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnModificar);
-            this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.dgvHabitaciones);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formHabitaciones";
@@ -192,8 +111,5 @@
         #endregion
 
         private Guna.UI.WinForms.GunaDataGridView dgvHabitaciones;
-        private Guna.UI.WinForms.GunaButton btnRegistrar;
-        private Guna.UI.WinForms.GunaButton btnModificar;
-        private Guna.UI.WinForms.GunaButton btnEliminar;
     }
 }
