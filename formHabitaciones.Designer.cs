0b2e097b5bc4b0ae8caf19ccfbdd79429bb7ac68
LuisMario698 <LuisMario698@hotmail.com>
2024-12-10 15:01:55 -0700

    er

--- formHabitaciones.Designer.cs
+++ formHabitaciones.Designer.cs
@@ -228,7 +228,7 @@
             this.txtPrecio.Name = "txtPrecio";
             this.txtPrecio.Size = new System.Drawing.Size(117, 20);
             this.txtPrecio.TabIndex = 11;
-            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            //this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
             // 
             // cmbTipo
             // 
